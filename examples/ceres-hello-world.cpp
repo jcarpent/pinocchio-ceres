0d5f171e6efdb4030315efd322cc87367f266d47
Justin Carpentier <jcarpent@laas.fr>
2018-09-17 15:10:01 +0200

    init: add first example of the project

---
+++ examples/ceres-hello-world.cpp
@@ -0,0 +1,62 @@
+#
+# Copyright (c) 2018 INRIA
+#
+# This file is part of Pinocchio-Ceres
+# pinocchio is free software: you can redistribute it
+# and/or modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation, either version
+# 3 of the License, or (at your option) any later version.
+# pinocchio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Lesser Public License for more details. You should have
+# received a copy of the GNU Lesser General Public License along with
+# pinocchio If not, see
+# <http://www.gnu.org/licenses/>.
+
+#include "ceres/ceres.h"
+#include "glog/logging.h"
+
+struct CostFunctor {
+  template <typename T>
+  bool operator()(const T* const x, T* residual) const {
+    residual[0] = T(10.0) - x[0];
+    return true;
+  }
+};
+
+int main(int argc, char** argv)
+{
+  using ceres::AutoDiffCostFunction;
+  using ceres::CostFunction;
+  using ceres::Problem;
+  using ceres::Solver;
+  using ceres::Solve;
+  
+  google::InitGoogleLogging(argv[0]);
+  
+  // The variable to solve for with its initial value.
+  double initial_x = 5.0;
+  double x = initial_x;
+  
+  // Build the problem.
+  Problem problem;
+  
+  // Set up the only cost function (also known as residual). This uses
+  // auto-differentiation to obtain the derivative (jacobian).
+  CostFunction* cost_function =
+  new AutoDiffCostFunction<CostFunctor, 1, 1>(new CostFunctor);
+  problem.AddResidualBlock(cost_function, NULL, &x);
+  
+  // Run the solver!
+  Solver::Options options;
+  options.linear_solver_type = ceres::DENSE_QR;
+  options.minimizer_progress_to_stdout = true;
+  Solver::Summary summary;
+  Solve(options, &problem, &summary);
+  
+  std::cout << summary.BriefReport() << "\n";
+  std::cout << "x : " << initial_x
+  << " -> " << x << "\n";
+  return 0;
+}
